31f398ce2d607906d86f7a9669384729bf2437b3
Celeste Zhu <celestezhuyy@gmail.com>
2026-07-23 19:17:30 +0900

    added enum for gender, goal, activity_level

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,6 +10,29 @@ class Profile < ApplicationRecord
   validate :must_have_cooking_device
   belongs_to :user
 
+  enum :gender, {
+    male: 0,
+    female: 1,
+    non_binary: 2,
+    prefer_not_to_say: 3
+  }
+
+  enum :goal, {
+    lose_weight: 0,
+    maintain_weight: 1,
+    gain_weight: 2,
+    build_muscle: 3,
+    eat_healthier: 4
+  }
+
+  enum :activity_level, {
+    sedentary: 0,
+    lightly_active: 1,
+    moderately_active: 2,
+    very_active: 3,
+    athlete: 4
+  }
+
   private
 
   def must_have_allergy
