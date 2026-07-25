65f0a4716e5202e7c28534137cd6b661ca3312d9
Myriam Bucourt <myriam.bucourt@gmail.com>
2026-07-25 12:10:47 +0900

    reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Protect every route by default
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
