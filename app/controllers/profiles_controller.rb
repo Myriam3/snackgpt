36d47024be6edb16d6b18a491139d34089d10235
Sean Taylor <seanmtaylor@me.com>
2026-07-30 20:53:07 +0900

    Allow custom profile allergies and cooking devices

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,25 @@ class ProfilesController < ApplicationController
   end
 
   def create
-    @profile = current_user.build_profile(profile_params)
+    attributes = profile_params
+
+    custom_allergies = attributes.delete(:custom_allergies)
+    custom_cooking_devices = attributes.delete(:custom_cooking_devices)
+
+    selected_allergy_ids = Array(attributes[:allergy_ids]).map(&:to_s)
+    selected_cooking_device_ids = Array(attributes[:cooking_device_ids]).map(&:to_s)
+
+    custom_allergy_ids = find_or_create_allergies(custom_allergies)
+    custom_cooking_device_ids = find_or_create_cooking_devices(custom_cooking_devices)
+
+    attributes[:allergy_ids] =
+      (selected_allergy_ids + custom_allergy_ids).uniq
+
+    attributes[:cooking_device_ids] =
+      (selected_cooking_device_ids + custom_cooking_device_ids).uniq
+
+    @profile = current_user.build_profile(attributes)
+
     if @profile.save
       GenerateNutritionPlanJob.perform_later(@profile.id)
       redirect_to profile_path
@@ -26,6 +44,38 @@ class ProfilesController < ApplicationController
 
   private
 
+  def find_or_create_allergies(names)
+    ids = []
+
+    Array(names).each do |name|
+      name = name.to_s.strip
+      next if name.blank?
+
+      allergy = Allergy.where("LOWER(name) = ?", name.downcase).first
+      allergy ||= Allergy.create!(name: name)
+
+      ids << allergy.id.to_s unless ids.include?(allergy.id.to_s)
+    end
+
+    ids
+  end
+
+  def find_or_create_cooking_devices(names)
+    ids = []
+
+    Array(names).each do |name|
+      name = name.to_s.strip
+      next if name.blank?
+
+      cooking_device = CookingDevice.where("LOWER(name) = ?", name.downcase).first
+      cooking_device ||= CookingDevice.create!(name: name)
+
+      ids << cooking_device.id.to_s unless ids.include?(cooking_device.id.to_s)
+    end
+
+    ids
+  end
+
   def profile_params
     params.expect(profile: [
                     :name,
@@ -38,7 +88,9 @@ class ProfilesController < ApplicationController
                     :preferences,
                     :conditions,
                     { allergy_ids: [] },
-                    { cooking_device_ids: [] }
+                    { cooking_device_ids: [] },
+                    { custom_allergies: [] },
+                    { custom_cooking_devices: [] }
                   ])
   end
 end
