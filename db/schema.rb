fae1682ff2b3f225489941c416a0025de785b24a
Myriam Bucourt <myriam.bucourt@gmail.com>
2026-08-01 13:49:50 +0900

    meal, dashboard, navbar, sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_01_021726) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_29_030521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,11 +60,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_01_021726) do
 
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.bigint "meal_id", null: false
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.index ["meal_id"], name: "index_chats_on_meal_id"
     t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
@@ -320,7 +318,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_01_021726) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
-  add_foreign_key "chats", "meals"
   add_foreign_key "chats", "users"
   add_foreign_key "daily_objectives", "profiles"
   add_foreign_key "meals", "profiles"
