860763351840220a1cccce7064de5b8d3f06f5fe
Celeste Zhu <celestezhuyy@gmail.com>
2026-08-01 13:06:48 +0900

    change to openai

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,4 +1,5 @@
 RubyLLM.configure do |config|
-  config.gemini_api_key = ENV['GEMINI_API_KEY']
-  config.default_model = "gemini-flash-latest"
+  config.openai_api_key = ENV["OPENAI_API_KEY"]
+  # config.gemini_api_key = ENV['GEMINI_API_KEY']
+  # config.default_model = "gemini-flash-latest"
 end
